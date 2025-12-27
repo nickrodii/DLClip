25fa199a8f1d90b92c3074b16480f99415b0dce0
nickrodii <nickjrodi@gmail.com>
2025-12-27 06:59:21 -0700

    some changes to mediaprobeservice

--- Services/MediaProbeService.cs
+++ Services/MediaProbeService.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using DLClip.Models;
 using DLClip.Utils;
 using System.IO;
+using System.Text.Json;
+using System.Globalization;
 
 namespace DLClip.Services
 {
@@ -24,14 +26,29 @@ namespace DLClip.Services
             // FFPROBE
             if (!usingURL)
             {
-                ProbeResult pr = await CLIUtils.RunProbe(ffprobeExe, new[] { "-v error", "-print_format json", "-show_format", "-show_streams" }, input, 60000);
+                ProbeResult pr = await CLIUtils.RunProbe(ffprobeExe, new[] { "-v", "error", "-print_format", "json", "-show_format", "-show_streams" }, input, 60000);
+                if (pr.ExitCode != 0 || pr.StdOut == null) { return new ProbeMediaResult { Success = false, ErrorTitle = "FFprobe Error", ErrorMessage = pr.StdErr }; }
 
+                ProbeMediaResult mediaInfo = new ProbeMediaResult();
+                JsonDocument ffprobeJson = JsonDocument.Parse(pr.StdOut);
+                var root = ffprobeJson.RootElement;
+                var format = root.GetProperty("format");
+                var streams = root.GetProperty("streams");
+
+                string durStr = format.GetProperty("duration").GetString() ?? throw new InvalidOperationException("Duration missing");
+                bool parseOk = double.TryParse(durStr, System.Globalization.NumberStyles.Float, CultureInfo.InvariantCulture, out double dur);
+                if (!parseOk) { return new ProbeMediaResult { Success = false, ErrorTitle = "FFprobe Error", ErrorMessage = "Malformed output" }; }
+                mediaInfo.DurationSeconds = (int)Math.Floor(dur);
             }
 
             // YT-DLP
             else
             {
-                await CLIUtils.RunProbe(ytdlpExe, new[] { "--dump-json", "--no-warnings", "--no-playlist" }, input, 60000);
+                ProbeResult pr = await CLIUtils.RunProbe(ytdlpExe, new[] { "--dump-json", "--no-warnings", "--no-playlist" }, input, 60000);
+                if (pr.ExitCode != 0)
+                {
+                    return new ProbeMediaResult { Success = false, ErrorTitle = "yt-dlp Error", ErrorMessage = pr.StdErr };
+                }
             }
 
             return new ProbeMediaResult { Success = false, ErrorTitle = "Not implemented", ErrorMessage = "Media probing is not yet implemented." };
